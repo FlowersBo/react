521253e9b5a05fe055f67d83fe0f716c9e6c2b04
flowers <172275766@qq.com>
2018-10-30 23:20:12 +0800

    修改了本地的index，只是个测试

--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ import ReactDOM from 'react-dom';
 import {Button} from 'antd-mobile';
 
 ReactDOM.render(
-  <Button type='primary'>按钮</Button>
-  , document.getElementById('root'));
+  <Button type='primary'>按钮</Button>, document.getElementById('root'));
 
